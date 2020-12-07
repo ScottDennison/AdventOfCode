1aa27671caa49239518f66453de90c9ad8023141
Scott Dennison <scott.dennison@jagex.com>
2020-12-07 03:26:55 +0000

    2017 days 01-02. Also placeholders for 2017-2019 day 06

--- src/main/java/uk/co/scottdennison/java/challenges/adventofcode/puzzles/year2017/Day01.java
+++ src/main/java/uk/co/scottdennison/java/challenges/adventofcode/puzzles/year2017/Day01.java
@@ -1,7 +1,43 @@
 package uk.co.scottdennison.java.challenges.adventofcode.puzzles.year2017;
 
+import uk.co.scottdennison.java.challenges.adventofcode.utils.InputFileUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
 public class Day01 {
-	public static void main(String[] args) {
-		//TODO
+	public static void main(String[] args) throws IOException {
+		char[] fileCharacters = new String(Files.readAllBytes(InputFileUtils.getInputPath()), StandardCharsets.UTF_8).trim().toCharArray();
+		int fileCharactersLength = fileCharacters.length;
+		int captcha2Offset = fileCharactersLength / 2;
+		int captcha1 = 0;
+		int captcha2 = 0;
+		for (int fileCharacterIndex = 0; fileCharacterIndex < fileCharactersLength; fileCharacterIndex++) {
+			captcha1 += checkCharacter(fileCharacters, fileCharacterIndex, 1);
+			captcha2 += checkCharacter(fileCharacters, fileCharacterIndex, captcha2Offset);
+		}
+		outputSummary(1, captcha1);
+		outputSummary(2, captcha2);
+	}
+
+	private static void outputSummary(int captchaNumber, int result) {
+		System.out.format("Captcha %d answer: %d%n", captchaNumber, result);
+	}
+
+	private static int checkCharacter(char[] fileCharacters, int currentIndex, int offset) {
+		char currentFileCharacter = fileCharacters[currentIndex];
+		char checkFileCharacter = fileCharacters[(currentIndex + offset) % fileCharacters.length];
+		if (currentFileCharacter == checkFileCharacter) {
+			if (currentFileCharacter >= '0' && currentFileCharacter <= '9') {
+				return (currentFileCharacter - '0');
+			}
+			else {
+				throw new IllegalStateException("Invalid file character.");
+			}
+		}
+		else {
+			return 0;
+		}
 	}
 }
--- src/main/java/uk/co/scottdennison/java/challenges/adventofcode/puzzles/year2017/Day02.java
+++ src/main/java/uk/co/scottdennison/java/challenges/adventofcode/puzzles/year2017/Day02.java
@@ -1,7 +1,55 @@
 package uk.co.scottdennison.java.challenges.adventofcode.puzzles.year2017;
 
+import uk.co.scottdennison.java.challenges.adventofcode.utils.InputFileUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.regex.Pattern;
+
 public class Day02 {
-	public static void main(String[] args) {
-		//TODO
+	private static final Pattern SPACE_PATTERN = Pattern.compile("\\s+");
+
+	public static void main(String[] args) throws IOException {
+		int checksum1 = 0;
+		int checksum2 = 0;
+		for (String fileLine : Files.readAllLines(InputFileUtils.getInputPath())) {
+			int minimumValue = Integer.MAX_VALUE;
+			int maximumValue = Integer.MIN_VALUE;
+			String[] values = SPACE_PATTERN.split(fileLine);
+			int valueCount = values.length;
+			int[] intValues = new int[valueCount];
+			int cleanDivisionResult = -1;
+			for (int newIndex = 0; newIndex < valueCount; newIndex++) {
+				int newIntValue = Integer.parseInt(values[newIndex]);
+				intValues[newIndex] = newIntValue;
+				for (int oldIndex = 0; oldIndex < newIndex; oldIndex++) {
+					int oldIntValue = intValues[oldIndex];
+					int smallerValue = Math.min(oldIntValue, newIntValue);
+					int largerValue = Math.max(oldIntValue, newIntValue);
+					int divisionResult = largerValue / smallerValue;
+					if ((divisionResult * smallerValue) == largerValue) {
+						if (cleanDivisionResult < 0) {
+							cleanDivisionResult = divisionResult;
+						}
+						else {
+							throw new IllegalStateException("Multiple values on this row divide cleanly.");
+						}
+					}
+				}
+				minimumValue = Math.min(minimumValue, newIntValue);
+				maximumValue = Math.max(maximumValue, newIntValue);
+			}
+			if (cleanDivisionResult < 0) {
+				throw new IllegalStateException("No values on this row divide cleanly.");
+			}
+			checksum1 += (maximumValue - minimumValue);
+			checksum2 += cleanDivisionResult;
+		}
+		outputSummary(1, checksum1);
+		outputSummary(2, checksum2);
+	}
+
+	private static void outputSummary(int checksumNumber, int checksum) {
+		System.out.format("Checksum %d is %d%n", checksumNumber, checksum);
 	}
 }
