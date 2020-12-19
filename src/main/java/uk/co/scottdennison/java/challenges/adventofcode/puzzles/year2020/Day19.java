6f50e97501898bf63d55f43acbe17f7c3efe5a19
Scott Dennison <scott.dennison@jagex.com>
2020-12-19 18:58:45 +0000

    2020 day 19. This version solves both part 1 and part 2.

--- src/main/java/uk/co/scottdennison/java/challenges/adventofcode/puzzles/year2020/Day19.java
+++ src/main/java/uk/co/scottdennison/java/challenges/adventofcode/puzzles/year2020/Day19.java
@@ -12,139 +12,235 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day19 {
+	private static final String[] EXTRA_RULE_LINES = {
+		"8: 42 | 42 8",
+		"11: 42 31 | 42 11 31"
+	};
+
 	private static final Pattern PATTERN_RULE = Pattern.compile("^(?<ruleNumber>[0-9]+): (?<ruleComponents>.+)$");
-	private static final Pattern PATTERN_RULE_COMPONENT_SPLIT = Pattern.compile(" ");
-	private static final Pattern PATTERN_RULE_COMPONENT = Pattern.compile("(?<otherRuleNumber>[0-9]+)|(?<or>\\|)|(?:\"(?<literal>.+?)\")");
+	private static final Pattern PATTERN_RULE_COMPONENT_OR_PART_SPLIT = Pattern.compile(" *\\| *");
+	private static final Pattern PATTERN_RULE_COMPONENT_SEQUENCE_SPLIT = Pattern.compile(" ");
+	private static final Pattern PATTERN_RULE_COMPONENT_PART = Pattern.compile("(?<otherRuleNumber>[0-9]+)|(?:\"(?<literal>.+?)\")");
+
+	private static final class EndRuleComponent implements RuleComponent {
+		public static final EndRuleComponent INSTANCE = new EndRuleComponent();
+
+		private EndRuleComponent() {
+		}
+
+		@Override
+		public boolean test(char[] input, int index, RuleComponent nextRuleComponent, NumberedRuleComponentManager numberedRuleComponentManager) {
+			if (nextRuleComponent != null) {
+				throw new IllegalStateException("An end rule component cannot be followed by another rule");
+			}
+			return index == input.length;
+		}
+	}
 
 	private static final class OrRuleComponent implements RuleComponent {
+		private final List<RuleComponent> ruleComponents;
+
+		private OrRuleComponent(List<RuleComponent> ruleComponents) {
+			this.ruleComponents = new ArrayList<>(ruleComponents);
+		}
+
 		@Override
-		public String buildRegularExpression(RuleManager ruleManager) {
-			return "|";
+		public boolean test(char[] input, int index, RuleComponent nextRuleComponent, NumberedRuleComponentManager numberedRuleComponentManager) {
+			for (RuleComponent ruleComponent : this.ruleComponents) {
+				if (ruleComponent.test(input, index, nextRuleComponent, numberedRuleComponentManager)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	private static final class SequenceRuleComponent implements RuleComponent {
+		private static final class TwoPartRuleComponent implements RuleComponent {
+			private final RuleComponent leftRuleComponent;
+			private final RuleComponent rightRuleComponent;
+
+			public TwoPartRuleComponent(RuleComponent leftRuleComponent, RuleComponent rightRuleComponent) {
+				this.leftRuleComponent = leftRuleComponent;
+				this.rightRuleComponent = rightRuleComponent;
+			}
+
+			@Override
+			public boolean test(char[] input, int index, RuleComponent nextRuleComponent, NumberedRuleComponentManager numberedRuleComponentManager) {
+				if (nextRuleComponent == null) {
+					nextRuleComponent = this.rightRuleComponent;
+				}
+				else {
+					nextRuleComponent = new TwoPartRuleComponent(this.rightRuleComponent, nextRuleComponent);
+				}
+				return this.leftRuleComponent.test(input, index, nextRuleComponent, numberedRuleComponentManager);
+			}
+		}
+
+		private final RuleComponent ruleComponent;
+
+		public SequenceRuleComponent(List<RuleComponent> ruleComponents) {
+			if (ruleComponents.isEmpty()) {
+				throw new IllegalStateException("Rule components cannot be empty");
+			}
+			int index = ruleComponents.size() - 1;
+			RuleComponent ruleComponent = ruleComponents.get(index);
+			while (--index >= 0) {
+				ruleComponent = new TwoPartRuleComponent(ruleComponents.get(index), ruleComponent);
+			}
+			this.ruleComponent = ruleComponent;
+		}
+
+		@Override
+		public boolean test(char[] input, int index, RuleComponent nextRuleComponent, NumberedRuleComponentManager numberedRuleComponentManager) {
+			return this.ruleComponent.test(input, index, nextRuleComponent, numberedRuleComponentManager);
 		}
 	}
 
 	private static final class LiteralRuleComponent implements RuleComponent {
-		private final String literalText;
+		private final char[] literalCharacters;
 
-
-		private LiteralRuleComponent(String literalText) {
-			this.literalText = literalText;
+		private LiteralRuleComponent(String literalString) {
+			this.literalCharacters = literalString.toCharArray();
 		}
 
 		@Override
-		public String buildRegularExpression(RuleManager ruleManager) {
-			return this.literalText;
+		public boolean test(char[] input, int index, RuleComponent nextRuleComponent, NumberedRuleComponentManager numberedRuleComponentManager) {
+			int literalCharacterCount = this.literalCharacters.length;
+			int nextRuleIndex = index + literalCharacterCount;
+			if (nextRuleIndex > input.length) {
+				return false;
+			}
+			for (int literalCharacterIndex = 0, inputIndex = index; literalCharacterIndex < literalCharacterCount; literalCharacterIndex++, inputIndex++) {
+				if (this.literalCharacters[literalCharacterIndex] != input[inputIndex]) {
+					return false;
+				}
+			}
+			return nextRuleComponent.test(input, nextRuleIndex, null, numberedRuleComponentManager);
 		}
 	}
 
-	private static final class OtherRuleRuleComponent implements RuleComponent {
-		private final int ruleNumber;
+	private static final class OtherRuleComponentRuleComponent implements RuleComponent {
+		private final int ruleComponentNumber;
 
-		public OtherRuleRuleComponent(int ruleNumber) {
-			this.ruleNumber = ruleNumber;
+		public OtherRuleComponentRuleComponent(int ruleComponentNumber) {
+			this.ruleComponentNumber = ruleComponentNumber;
 		}
 
 		@Override
-		public String buildRegularExpression(RuleManager ruleManager) {
-			return ruleManager.getRule(ruleNumber).buildRegularExpression(ruleManager);
+		public boolean test(char[] input, int index, RuleComponent nextRuleComponent, NumberedRuleComponentManager numberedRuleComponentManager) {
+			return numberedRuleComponentManager.getRuleComponent(this.ruleComponentNumber).test(input, index, nextRuleComponent, numberedRuleComponentManager);
 		}
 	}
 
 	private interface RuleComponent {
-		String buildRegularExpression(RuleManager ruleManager);
+		boolean test(char[] input, int index, RuleComponent nextRuleComponent, NumberedRuleComponentManager numberedRuleComponentManager);
 	}
 
-	private static final class Rule {
-		private final List<RuleComponent> ruleComponents;
+	private static final class NumberedRuleComponentManager {
+		private final Map<Integer, RuleComponent> ruleComponents = new HashMap<>();
 
-		private Rule(List<RuleComponent> ruleComponents) {
-			this.ruleComponents = new ArrayList<>(ruleComponents);
-		}
-
-		public String buildRegularExpression(RuleManager ruleManager) {
-			StringBuilder stringBuilder = new StringBuilder();
-			stringBuilder.append('(');
-			for (RuleComponent ruleComponent : this.ruleComponents) {
-				stringBuilder.append(ruleComponent.buildRegularExpression(ruleManager));
-			}
-			stringBuilder.append(')');
-			return stringBuilder.toString();
-		}
-	}
-
-	private static final class RuleManager {
-		private final Map<Integer, Rule> rules = new HashMap<>();
-
-		public void addRule(int ruleNumber, Rule rule) {
-			if (this.rules.put(ruleNumber, rule) != null) {
+		public void addRuleComponent(int ruleNumber, RuleComponent ruleComponent, boolean allowOverride) {
+			if ((this.ruleComponents.put(ruleNumber, ruleComponent) != null) ^ allowOverride) {
 				throw new IllegalStateException("Duplicate rule");
 			}
 		}
 
-		public Rule getRule(int ruleNumber) {
-			Rule rule = this.rules.get(ruleNumber);
-			if (rule == null) {
-				throw new IllegalStateException("No such rule");
+		public RuleComponent getRuleComponent(int ruleNumber) {
+			RuleComponent ruleComponent = this.ruleComponents.get(ruleNumber);
+			if (ruleComponent == null) {
+				throw new IllegalStateException("No such rule component");
 			}
-			return rule;
+			return ruleComponent;
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
-		boolean parsingRules = true;
-		RuleManager ruleManager = new RuleManager();
-		Pattern rule0Pattern = null;
-		int matchCount = 0;
-		for (String fileLine : Files.readAllLines(InputFileUtils.getInputPath())) {
-			if (parsingRules) {
-				if (fileLine.isEmpty()) {
-					parsingRules = false;
-					rule0Pattern = Pattern.compile(ruleManager.getRule(0).buildRegularExpression(ruleManager));
+	private static void parseRuleIntoManager(NumberedRuleComponentManager numberedRuleComponentManager, String line, boolean allowOverride) {
+		Matcher ruleMatcher = PATTERN_RULE.matcher(line);
+		if (!ruleMatcher.matches()) {
+			throw new IllegalStateException("Could not parse rule");
+		}
+		List<RuleComponent> ruleComponents = new ArrayList<>();
+		for (String ruleComponentOrPartString : PATTERN_RULE_COMPONENT_OR_PART_SPLIT.split(ruleMatcher.group("ruleComponents"))) {
+			List<RuleComponent> ruleComponentsInOrPart = new ArrayList<>();
+			for (String ruleComponentSequencePartString : PATTERN_RULE_COMPONENT_SEQUENCE_SPLIT.split(ruleComponentOrPartString)) {
+				Matcher ruleComponentMatcher = PATTERN_RULE_COMPONENT_PART.matcher(ruleComponentSequencePartString);
+				if (!ruleComponentMatcher.matches()) {
+					throw new IllegalStateException("Could not parse rule component");
+				}
+				String otherRuleNumberString = ruleComponentMatcher.group("otherRuleNumber");
+				RuleComponent ruleComponent;
+				if (otherRuleNumberString != null) {
+					ruleComponent = new OtherRuleComponentRuleComponent(Integer.parseInt(otherRuleNumberString));
 				}
 				else {
-					Matcher ruleMatcher = PATTERN_RULE.matcher(fileLine);
-					if (!ruleMatcher.matches()) {
-						throw new IllegalStateException("Could not parse rule");
+					String literalString = ruleComponentMatcher.group("literal");
+					if (literalString != null) {
+						ruleComponent = new LiteralRuleComponent(literalString);
 					}
-					List<RuleComponent> ruleComponents = new ArrayList<>();
-					for (String ruleComponentString : PATTERN_RULE_COMPONENT_SPLIT.split(ruleMatcher.group("ruleComponents"))) {
-						Matcher ruleComponentMatcher = PATTERN_RULE_COMPONENT.matcher(ruleComponentString);
-						if (!ruleComponentMatcher.matches()) {
-							throw new IllegalStateException("Could not parse rule component");
-						}
-						String otherRuleNumberString = ruleComponentMatcher.group("otherRuleNumber");
-						RuleComponent ruleComponent;
-						if (otherRuleNumberString != null) {
-							ruleComponent = new OtherRuleRuleComponent(Integer.parseInt(otherRuleNumberString));
-						}
-						else {
-							String orString = ruleComponentMatcher.group("or");
-							if (orString != null) {
-								ruleComponent = new OrRuleComponent();
-							}
-							else {
-								String literalString = ruleComponentMatcher.group("literal");
-								if (literalString != null) {
-									ruleComponent = new LiteralRuleComponent(literalString);
-								}
-								else {
-									throw new IllegalStateException("Could not extract rule component data");
-								}
-							}
-						}
-						ruleComponents.add(ruleComponent);
+					else {
+						throw new IllegalStateException("Could not extract rule component data");
 					}
-					ruleManager.addRule(Integer.parseInt(ruleMatcher.group("ruleNumber")), new Rule(ruleComponents));
+				}
+				ruleComponentsInOrPart.add(ruleComponent);
+			}
+			RuleComponent ruleComponentOrPart;
+			if (ruleComponentsInOrPart.size() == 1) {
+				ruleComponentOrPart = ruleComponentsInOrPart.get(0);
+			}
+			else {
+				ruleComponentOrPart = new SequenceRuleComponent(ruleComponentsInOrPart);
+			}
+			ruleComponents.add(ruleComponentOrPart);
+		}
+		RuleComponent ruleComponent;
+		if (ruleComponents.size() == 1) {
+			ruleComponent = ruleComponents.get(0);
+		}
+		else {
+			ruleComponent = new OrRuleComponent(ruleComponents);
+		}
+		numberedRuleComponentManager.addRuleComponent(Integer.parseInt(ruleMatcher.group("ruleNumber")), ruleComponent, allowOverride);
+	}
+
+	private static int matchLinesAgainstRule(NumberedRuleComponentManager numberedRuleComponentManager, int ruleNumber, List<String> inputLines) {
+		RuleComponent ruleComponent = numberedRuleComponentManager.getRuleComponent(ruleNumber);
+		int matchCount = 0;
+		for (String inputLine : inputLines) {
+			if (ruleComponent.test(inputLine.toCharArray(), 0, EndRuleComponent.INSTANCE, numberedRuleComponentManager)) {
+				matchCount++;
+			}
+		}
+		return matchCount;
+	}
+
+	public static void main(String[] args) throws IOException {
+		NumberedRuleComponentManager numberedRuleComponentManager = new NumberedRuleComponentManager();
+		List<String> inputLines = new ArrayList<>();
+		boolean rulesFinished = false;
+		for (String fileLine : Files.readAllLines(InputFileUtils.getInputPath())) {
+			if (rulesFinished) {
+				if (!fileLine.isEmpty()) {
+					inputLines.add(fileLine);
 				}
 			}
 			else {
-				System.out.println(fileLine);
-				if (!fileLine.isEmpty() && rule0Pattern.matcher(fileLine).matches()) {
-					System.out.println("\tMatches");
-					matchCount++;
+				if (fileLine.isEmpty()) {
+					rulesFinished = true;
+				}
+				else {
+					parseRuleIntoManager(numberedRuleComponentManager, fileLine, false);
 				}
 			}
 		}
-		System.out.format("Match count: %d%n", matchCount);
+		outputSummary(1, numberedRuleComponentManager, 0, inputLines);
+		for (String extraRuleLine : EXTRA_RULE_LINES) {
+			parseRuleIntoManager(numberedRuleComponentManager, extraRuleLine, true);
+		}
+		outputSummary(2, numberedRuleComponentManager, 0, inputLines);
+	}
+
+	private static void outputSummary(int partNumber, NumberedRuleComponentManager numberedRuleComponentManager, @SuppressWarnings("SameParameterValue") int ruleNumber, List<String> inputLines) {
+		System.out.format("Match count for part %d: %d%n", partNumber, matchLinesAgainstRule(numberedRuleComponentManager, ruleNumber, inputLines));
 	}
 }
