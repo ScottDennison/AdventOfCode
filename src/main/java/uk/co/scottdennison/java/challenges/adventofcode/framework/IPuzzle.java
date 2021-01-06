b1dd56139f4d7eb00c3bd6013b727b92b2aba741
Scott Dennison <scott.dennison@jagex.com>
2021-01-06 13:41:01 +0000

    Partial progress of converting to IPuzzle format

---
+++ src/main/java/uk/co/scottdennison/java/challenges/adventofcode/framework/IPuzzle.java
@@ -0,0 +1,5 @@
+package uk.co.scottdennison.java.challenges.adventofcode.framework;
+
+public class IPuzzle {
+	IPuzzleResults runPuzzle(byte[] input, IConfigProvider configProvider, PrintWriter outputWriter)
+}
