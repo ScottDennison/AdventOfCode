39148ac1b7dd3ddf0b51be2851974bdda5987746
Scott Dennison <darthcrap@hotmail.co.uk>
2024-12-15 05:15:46 +0000

    2024 day 15 data. Only empty solution shell currently

---
+++ src/main/java/uk/co/scottdennison/java/soft/challenges/adventofcode/puzzles/year2024/Day15.java
@@ -0,0 +1,14 @@
+package uk.co.scottdennison.java.soft.challenges.adventofcode.puzzles.year2024;
+
+import uk.co.scottdennison.java.soft.challenges.adventofcode.framework.IPuzzle;
+import uk.co.scottdennison.java.soft.challenges.adventofcode.framework.IPuzzleConfigProvider;
+import uk.co.scottdennison.java.soft.challenges.adventofcode.framework.IPuzzleResults;
+
+import java.io.PrintWriter;
+
+public class Day15 implements IPuzzle {
+    @Override
+    public IPuzzleResults runPuzzle(char[] inputCharacters, IPuzzleConfigProvider configProvider, boolean partBPotentiallyUnsolvable, PrintWriter printWriter) {
+        return null;
+    }
+}
