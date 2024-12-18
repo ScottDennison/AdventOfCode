a73a087ee6d83933b5a8d38916e94420a0885ef4
Scott Dennison <darthcrap@hotmail.co.uk>
2024-12-18 21:38:13 +0000

    2024 day 18 - and another breakthrough takes us under 100ms!

--- src/main/java/uk/co/scottdennison/java/soft/challenges/adventofcode/puzzles/year2024/Day18.java
+++ src/main/java/uk/co/scottdennison/java/soft/challenges/adventofcode/puzzles/year2024/Day18.java
@@ -8,9 +8,11 @@ import uk.co.scottdennison.java.soft.challenges.adventofcode.framework.IPuzzleCo
 import uk.co.scottdennison.java.soft.challenges.adventofcode.framework.IPuzzleResults;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -36,13 +38,7 @@ public class Day18 implements IPuzzle {
 
     private static final Pattern PATTERN_LINE = Pattern.compile("^(?<x>[0-9]+),(?<y>[0-9]+)$");
 
-    private static int runPartA(Coordinate[] fallingByteCoordinates, int gridSize, int partABytesFallen) {
-        boolean[][] corruptedSquares = new boolean[gridSize][gridSize];
-        int maxCoordinateNumber = gridSize - 1;
-        for (int fallingByteIndex=0; fallingByteIndex<partABytesFallen; fallingByteIndex++) {
-            Coordinate fallingByteCoordinate = fallingByteCoordinates[fallingByteIndex];
-            corruptedSquares[fallingByteCoordinate.getY()][fallingByteCoordinate.getX()] = true;
-        }
+    private static <ResultType> ResultType runAStar(boolean[][] corruptedSquares, int maxCoordinateNumber, AStarSolver.FullResultAdapter<AStarSolver.PointNodeAdapter.Point,Integer,ResultType> resultAdapter) {
         return AStarSolver.run(
             new AStarSolver.PointNodeAdapter<Integer>(
                 new AStarSolver.PointNodeAdapter.CanMoveAdapter() {
@@ -59,51 +55,38 @@ public class Day18 implements IPuzzle {
                 maxCoordinateNumber
             ),
             AStarSolver.CostAdapter.CommonTypes.Of.Integer.INSTANCE,
-            new AStarSolver.ThrowingResultAdapter<>(new AStarSolver.CostOnlyResultAdapter<>()),
+            resultAdapter,
             new AStarSolver.PointNodeAdapter.Point(0,0),
             new AStarSolver.PointNodeAdapter.Point(maxCoordinateNumber,maxCoordinateNumber)
         );
     }
 
+    private static int runPartA(Coordinate[] fallingByteCoordinates, int gridSize, int partABytesFallen) {
+        boolean[][] corruptedSquares = new boolean[gridSize][gridSize];
+        int maxCoordinateNumber = gridSize - 1;
+        AStarSolver.FullResultAdapter<AStarSolver.PointNodeAdapter.Point,Integer,Integer> resultAdapter = new AStarSolver.ThrowingResultAdapter<>(new AStarSolver.CostOnlyResultAdapter<>());
+        for (int fallingByteIndex=0; fallingByteIndex<partABytesFallen; fallingByteIndex++) {
+            Coordinate fallingByteCoordinate = fallingByteCoordinates[fallingByteIndex];
+            corruptedSquares[fallingByteCoordinate.getY()][fallingByteCoordinate.getX()] = true;
+        }
+        return runAStar(corruptedSquares, maxCoordinateNumber, resultAdapter);
+    }
+
     private static String runPartB(Coordinate[] fallingByteCoordinates, int gridSize) {
         boolean[][] corruptedSquares = new boolean[gridSize][gridSize];
         int maxCoordinateNumber = gridSize - 1;
-        Coordinate startCordinate = new Coordinate(0,0);
-        Deque<Coordinate> coordinatesToVisit = new LinkedList<>();
-        fallingByteLoop:
+        AStarSolver.FullResultAdapter<AStarSolver.PointNodeAdapter.Point,Integer,Optional<AStarSolver.PointNodeAdapter.Point[]>> resultAdapter = new AStarSolver.OptionalResultAdapter<>(new AStarSolver.SingleRouteResultAdapter<>(AStarSolver.PointNodeAdapter.Point.class, AStarSolver.SingleRouteResultAdapter.MultiplePossibleRoutesBehaviour.PICK_ARTIBTARILY));
+        Set<AStarSolver.PointNodeAdapter.Point> currentRoutePoints = null;
         for (Coordinate fallingByteCoordinate : fallingByteCoordinates) {
             int fallingByteCoordinateX = fallingByteCoordinate.getX();
             int fallingByteCoordinateY = fallingByteCoordinate.getY();
             corruptedSquares[fallingByteCoordinateY][fallingByteCoordinateX] = true;
-            boolean[][] coordinatesVisited = new boolean[gridSize][gridSize];
-            coordinatesToVisit.clear();
-            coordinatesToVisit.addLast(startCordinate);
-            Coordinate coordinateToVisit;
-            while ((coordinateToVisit = coordinatesToVisit.poll()) != null) {
-                int coordinateToVisitX = coordinateToVisit.getX();
-                int coordinateToVisitY = coordinateToVisit.getY();
-                if (corruptedSquares[coordinateToVisitY][coordinateToVisitX] || coordinatesVisited[coordinateToVisitY][coordinateToVisitX]) {
-                    continue;
+            if (currentRoutePoints == null || currentRoutePoints.contains(new AStarSolver.PointNodeAdapter.Point(fallingByteCoordinateY, fallingByteCoordinateX))) {
+                Optional<AStarSolver.PointNodeAdapter.Point[]> aStarResult = runAStar(corruptedSquares, maxCoordinateNumber, resultAdapter);
+                if (!aStarResult.isPresent()) {
+                    return fallingByteCoordinateX + "," + fallingByteCoordinateY;
                 }
-                if (coordinateToVisitX == maxCoordinateNumber && coordinateToVisitY == maxCoordinateNumber) {
-                    continue fallingByteLoop;
-                }
-                coordinatesVisited[coordinateToVisitY][coordinateToVisitX] = true;
-                if (coordinateToVisitX > 0) {
-                    coordinatesToVisit.addLast(new Coordinate(coordinateToVisitX - 1, coordinateToVisitY));
-                }
-                if (coordinateToVisitX < maxCoordinateNumber) {
-                    coordinatesToVisit.addLast(new Coordinate(coordinateToVisitX + 1, coordinateToVisitY));
-                }
-                if (coordinateToVisitY > 0) {
-                    coordinatesToVisit.addLast(new Coordinate(coordinateToVisitX, coordinateToVisitY - 1));
-                }
-                if (coordinateToVisitY < maxCoordinateNumber) {
-                    coordinatesToVisit.addLast(new Coordinate(coordinateToVisitX, coordinateToVisitY + 1));
-                }
-            }
-            if (!coordinatesVisited[maxCoordinateNumber][maxCoordinateNumber]) {
-                return fallingByteCoordinateX + "," + fallingByteCoordinateY;
+                currentRoutePoints = new HashSet<>(Arrays.asList(aStarResult.get()));
             }
         }
         throw new IllegalStateException("The end coordinate is still reachable from the start coordinate after all falling bytes have fallen.");
